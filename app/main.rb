638d4b95019c1f9ce1f25b73633cecbab11585af
clair13 <theclairvoyant1313@yahoo.gr>
2026-06-18 06:40:56 +0300

    codecrafters submit [skip ci]

--- app/main.rb
+++ app/main.rb
@@ -1,3 +1,8 @@
 # TODO: Uncomment the code below to pass the first stage
 $stdout.write("$ ")
 
+command = gets
+
+command = command ? command.chomp : ""
+$stdout.puts("#{command}: command not found")
+
